f5ece6c0edd4e9a2169e938be11d02f6641b7999
Patrick Nekel <nekelpatrick@gmail.com>
2022-08-03 10:59:41 -0300

    :sparkles: add userData and stackData

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Samuel Leão",
+  githubUser: "samuelleaokenzie",
+  linkedinUser: "samuelleao",
+  whatsappNumber: "+5541111111111",
+  emailUser: "samuel@kenzie.com.br",
+  instagramUser: "samuelleaoui",
+  facebookUser: "",
+};
