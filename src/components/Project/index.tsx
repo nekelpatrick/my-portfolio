73e00938f72d6d7a758f28c85181e1dc9ca6fd71
Patrick Nekel <nekelpatrick@gmail.com>
2022-08-03 15:54:27 -0300

    :bug: fix links

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -17,7 +17,7 @@ interface ReposType {
   name: string;
   language: string;
   description: string;
-  git_url: string;
+  html_url: string;
   homepage: string;
 }
 
@@ -76,14 +76,14 @@ export const Project = (): JSX.Element => {
             {repository.description?.substring(0, 129)}
           </Text>
           <ProjectLinks>
-            <ProjectLink target="_blank" href={repository.git_url}>
+            <ProjectLink target="_blank" href={repository.html_url}>
               <FaGithub /> Github Code
             </ProjectLink>
-            {repository.homepage && (
+            {/* {repository.homepage && (
               <ProjectLink target="_blank" href={repository.homepage}>
                 <FaShare /> Aplicação
               </ProjectLink>
-            )}
+            )} */}
           </ProjectLinks>
         </ProjectWrapper>
       ))}
