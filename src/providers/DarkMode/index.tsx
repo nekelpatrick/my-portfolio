9a1767f8f0b4da20330b876e823aac6f22167551
Patrick Nekel <nekelpatrick@gmail.com>
2022-08-10 18:07:21 -0300

    :bug: fix darkmode bug

--- src/providers/DarkMode/index.tsx
+++ src/providers/DarkMode/index.tsx
@@ -5,30 +5,28 @@ import {
   useState,
   useEffect,
 } from "react";
-
 const ThemeContext = createContext<any | unknown>(undefined);
-
 type Props = {
   children: ReactNode;
 };
-
 export default function ThemeProvider({ children }: Props): JSX.Element {
-  const [darkMode, setDarkMode] = useState<Boolean>(false);
-
+  const [darkMode, setDarkMode] = useState(false);
   useEffect(() => {
-    const saveMode = JSON.parse(
-      window.localStorage.getItem("dark-mode") || "true"
-    );
-    saveMode && setDarkMode(saveMode);
+    let savemodeValue = JSON.parse(String(window.localStorage.getItem("dark-mode")))
+    if (window.localStorage.getItem("dark-mode") !== null) {
+      setDarkMode(savemodeValue);
+    }
+  }, []);
+  
+  useEffect(() => {
+    window.localStorage.setItem("dark-mode", JSON.stringify(darkMode));
   }, [darkMode]);
-
   return (
     <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
       {children}
     </ThemeContext.Provider>
   );
 }
-
 export function useThemeProvider() {
   const context = useContext(ThemeContext);
   return context;
